893223d73c85dbf5ac30d6c01abe2b18072bb3b2
Hector rojas <hrpxa270@hotmail.com>
2020-07-22 03:43:49 -0700

    Add rollup and babel with react

---
+++ rollup.config.js
@@ -0,0 +1,92 @@
+// export default {
+//   input: 'src/index.js',
+//   output: {
+//     file: 'dist/bundle.js',
+//     format: 'umd'
+//   }
+// }
+
+/*
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+
+const dist = 'dist';
+const bundle = 'bundle';
+const production = !process.env.ROLLUP_WATCH;
+
+export default {
+  input: 'src/index.js',
+  external: ['react'],
+  output: [
+    {
+      file: `${dist}/${bundle}.cjs.js`,
+      format: 'cjs'
+    },
+    {
+      file: `${dist}/${bundle}.esm.js`,
+      format: 'esm'
+    },
+    {
+      name: 'ReactCssSpinners',
+      file: `${dist}/${bundle}.umd.js`,
+      globals: {
+        react: 'React'
+      },
+      format: 'umd'
+    }
+  ],
+  plugins: [
+    resolve(),
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    // terser(),
+    production && terser()
+  ]
+}
+*/
+
+import babel from "rollup-plugin-babel";
+import resolve from "rollup-plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
+
+const dist = "dist";
+const bundle = "bundle";
+const production = !process.env.ROLLUP_WATCH;
+
+const outputs = [
+  {
+    file: `${dist}/${bundle}.cjs.js`,
+    format: "cjs",
+  },
+  {
+    file: `${dist}/${bundle}.esm.js`,
+    format: "esm",
+  },
+  {
+    name: "ReactCssSpinners",
+    file: `${dist}/${bundle}.umd.js`,
+    globals: {
+      react: "React",
+    },
+    format: "umd",
+  },
+];
+
+const common = {
+  input: "src/index.js",
+  external: ["react"],
+  plugins: [
+    resolve(),
+    babel({
+      exclude: "node_modules/**",
+    }),
+    production && terser(),
+  ],
+};
+
+export default outputs.map((output) => ({
+  ...common,
+  output,
+}));
